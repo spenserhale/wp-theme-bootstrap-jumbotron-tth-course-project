c60302abbeedfd593fbf654137b067e95f7b466a
Spenser Hale <spenserhale@outlook.com>
2014-11-04 09:15:47 -0800

    removed hard coded scripts in theme and added them to enqueue instead

--- footer.php
+++ footer.php
@@ -4,14 +4,6 @@
 	<p>&copy; Company 2014</p>
 </footer>
 </div> <!-- /container -->
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="../../dist/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -5,3 +5,17 @@ function theme_styles() {
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue', 'theme_styles');
+
+function theme_scripts() {
+
+	global $wp_scripts;
+
+	wp_register_script( 'html5_shiv', 'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js', null, null, false);
+	wp_register_script( 'respond_js', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', null, null, false);
+
+	$wp_scripts->add_data( 'html5_shiv','conditional', 'lt IE 9');
+	$wp_scripts->add_data( 'respond_js','conditional', 'lt IE 9');
+
+	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue', 'theme_scripts');
