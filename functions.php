5675bc43ba64da0df7e0a95cd096aa71f3956fcf
Spenser Hale <spenserhale@outlook.com>
2014-11-04 10:10:43 -0800

    added wp_nav_menu to theme

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
 
+add_theme_support('menus');
+
+function register_theme_menus() {
+	register_nav_menus(
+		array(
+			'header-menu' 	=> __( 'Header Menu' )
+		)
+	);
+}
+add_action( 'init', 'register_theme_menus' );
+
 function theme_styles() {
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css');
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
 }
-add_action('wp_enqueue', 'theme_styles');
+add_action('wp_enqueue_scripts', 'theme_styles');
 
 function theme_scripts() {
 
@@ -18,4 +29,6 @@ function theme_scripts() {
 
 	wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), null, true);
 }
-add_action('wp_enqueue', 'theme_scripts');
+add_action('wp_enqueue_scripts', 'theme_scripts');
+
+//add_filter('show_admin_bar', '__return_false');
--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 	<?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 	<div class="container">
@@ -27,15 +27,14 @@
 			<a class="navbar-brand" href="#">Project name</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
-			<form class="navbar-form navbar-right" role="form">
-				<div class="form-group">
-					<input type="text" placeholder="Email" class="form-control">
-				</div>
-				<div class="form-group">
-					<input type="password" placeholder="Password" class="form-control">
-				</div>
-				<button type="submit" class="btn btn-success">Sign in</button>
-			</form>
+			<?php
+			$defaults = array(
+				'theme_location'  => 'header-menu',
+				'container'       => false,
+				'menu_class'      => 'nav navbar-nav'
+			);
+			wp_nav_menu( $defaults );
+			?>
 		</div><!--/.navbar-collapse -->
 	</div>
 </nav>
