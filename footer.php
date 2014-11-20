8fcf4cf268eb8d525500de8763cc5d1c3200b6af
Spenser Hale <spenserhale@outlook.com>
2014-11-20 08:44:59 -0800

    added dynamic information in place of hard coded, such as copyright, title, etc

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <hr>
 
 <footer>
-	<p>&copy; Company 2014</p>
+	<p>&copy; <?php bloginfo('name');?> <?php echo date('Y'); ?> 2014</p>
 </footer>
 </div> <!-- /container -->
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -4,11 +4,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
-	<link rel="icon" href="../../favicon.ico">
+	<link rel="icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico">
 
-	<title>Jumbotron Template for Bootstrap</title>
+	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
 	<?php wp_head(); ?>
 </head>
